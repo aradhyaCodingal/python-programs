8bbedf0642c7ff5d0e62b5a0ed6cc99c4d1008da
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-07-02 20:38:07 +0530

    data type

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+num=20
+print("type of num variable",type(num))
+str="hello world"
+print("type of str variable",type(str))
+age=12.4
+print("type of age variable",type(age))
+bol=True
+print("type of bol variable",type(bol))
