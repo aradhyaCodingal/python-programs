41e3380ba3bbba261bc8f7bea613cff961b65e81
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-07-13 11:08:00 +0530

    persentage

---
+++ persentage.py
@@ -0,0 +1,14 @@
+mathematics=85
+science=92
+english=93
+geography=96
+history=89
+print("mathematics",mathematics)
+print("science",science)
+print("english",english)
+print("geography",geography)
+print("history",history)
+total=mathematics+science+english+geography+history
+print("total marks=",total)
+average=(total/500)*100
+print("average marks",average)
