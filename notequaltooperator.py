8247cf3850492d90b8e9ad64be793c5598974cfb
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-07-23 20:34:37 +0530

    not equal to operator

---
+++ notequaltooperator.py
@@ -0,0 +1,11 @@
+num1=5
+num2=2
+num3=2
+print(num1 != num2)
+print(num2 != num3)
+str1="coding"
+str2="python"
+if str1 != str2:
+    print("both the values are not equal")
+if (num1 >5)!=(num2 >5) :
+    priint("output of both conditions are not different")
