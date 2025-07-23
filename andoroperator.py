76a0c95202049f93bd1eb7b2e0af804f309bed27
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-07-23 20:15:40 +0530

    and or operators

---
+++ andoroperator.py
@@ -0,0 +1,11 @@
+num1=7
+num2=-2
+if num1> 0 and num2>0:
+    print("both are true")
+else:
+    print("either of then is false")
+if num1>0 or num2>0:
+    print("either of then is true")
+else:
+    print("either of them is false")
+    
