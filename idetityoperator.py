a5e8b2e406e9164e8a86bfb19074a35fa9356afe
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-07-30 20:08:57 +0530

    identity operator

---
+++ idetityoperator.py
@@ -0,0 +1,25 @@
+a=10
+if type(a) is int:
+    print("the statement is true")
+else:
+    print("the statement is false")
+
+b=3.5
+if type (b) is int:
+    print("statement is true")
+else:
+    print("statement is false")
+    
+x=10
+y=10
+if (x is y):
+    print("both are same")
+else:
+    print("both are not same")
+
+y=30
+if (y is not x):
+    print("they are not the same")
+
+
+   
