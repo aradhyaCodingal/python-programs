e7fe63cd2e1a0c9ea3d93fee3e0351d83ae95ffd
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-08-06 19:57:18 +0530

    operator precedence

---
+++ operatorprecedence.py
@@ -0,0 +1,14 @@
+v=4
+w=5
+x=8
+y=2
+z=(v+w)*x/y
+print("the result is ",z)
+
+name="aradhya"
+age= 27
+if name=="aradhya"and age==27:
+    print("name=",name,"äge=",age)
+else:
+    print("none of them match")
+
