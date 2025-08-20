efc7b99f5b86f05ec7db15add4c11963526a90da
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-08-20 19:57:51 +0530

    sum of whole numbers

---
+++ agechecker.py
@@ -0,0 +1,9 @@
+age=int(input("enter your age"))
+if age==10:
+    print("allowed to write exam")
+    if age<=20:
+        print("allowed to write exam")
+    else:
+        print("invalid input")
+else:
+    print("invalid input")
