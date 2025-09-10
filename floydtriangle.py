4169bedc41870f05a66a6f378179292818bc22ce
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-09-10 19:57:55 +0530

    floyd numbers

---
+++ floydtriangle.py
@@ -0,0 +1,11 @@
+row=int(input("enter the number of rows"))
+num=1
+for i in range(row+1):
+    for j in range(1,i+1):
+        print(num, end=" ")
+        num=num+1
+    print()
+
+        
+     
+     
