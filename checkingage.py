70c881454fa1895db7aaacba36ec6dab2bb66b6e
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-09-12 09:14:38 +0530

    checking age

---
+++ checkingage.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age"))
+if age>=10:
+    if age<=20:
+        print("you are allwed to go to this class")
+    else:
+        print("you are not allowed to go to this class")
+else:
+    print("you are not allowed to go to class")
