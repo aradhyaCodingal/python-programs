f06ea86848fefa7215c9b7e365d7f3d80b6ca67f
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-11-19 19:45:02 +0530

    exceptions

---
+++ exeption.py
@@ -0,0 +1,9 @@
+valid=False
+while not valid:
+    try:
+        num=int(input("enter a number"))
+        while num%2==0:
+            print("bye")
+        valid=True
+    except ValueError:
+        print("exeption")
