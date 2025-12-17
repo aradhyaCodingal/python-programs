229efd0d9301dc6609ca8bccc72e10165a4ca4fe
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-12-17 20:33:25 +0530

    weather

---
+++ weather.py
@@ -0,0 +1,12 @@
+weather=(1,0,0,0,1,1,0)
+sunny=0
+rainy=0
+for i in weather:
+    if weather[i]==0:
+        sunny=sunny+1
+    else:
+        rainy=rainy+1
+if (sunny > rainy):
+    print("good weather")
+else:
+    print("bad weather")
