e4cc42f3a4b6fc84f80cf858475af0a6f0d0a81a
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2025-12-31 16:58:16 +0530

    country code

---
+++ country.py
@@ -0,0 +1,5 @@
+country_code={'india':'0091','australia':'0025','nepal':'00977'}
+print("return country code for india")
+print(country_code.get('india','not found'))
+print("return country codde for japan")
+print(country_code.get('japan','not found'))
