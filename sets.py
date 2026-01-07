15aa9f7b1d12f19ca8943f94727490ef6d12c569
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2026-01-07 19:49:18 +0530

    set

---
+++ sets.py
@@ -0,0 +1,12 @@
+set_1 ={1,2,3,4,5}
+print(set_1)
+set_2={12.4,"hello",(7,8,9)}
+print(set_2)
+set_3={1,2,3,4,3,2}
+print(set_3)
+set_4 = set([1,2,3,2])
+print(set_4)
+set_5=set([0,1,3,4,5])
+print("original set",set_5)
+set_5.pop()
+print("the set after popping the element",set_5)
