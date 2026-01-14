bd686970afb6ef64fecfd7eb978fe0030558dcee
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2026-01-14 19:46:35 +0530

    map

---
+++ map.py
@@ -0,0 +1,10 @@
+n1=[1,2,3]
+n2=[4,5,6]
+reasult=map(lambda x,y:x+y,n1,n2)
+print("addition is :")
+print(list(reasult))
+num=[1,2,3,4,5,6]
+def square(n):
+    return n * n
+sq=list(map(square,num))
+print(sq)
