137eaae54b57a99234ef5be2afb7f8cbd2d9bc6b
aradhyaCodingal <patnaik.aradhya18@gmail.com>
2026-02-25 19:54:50 +0530

    employeee

---
+++ employee.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self):
+        print("employee created")
+    def __init__(self):
+        print("employee deleted")
+def create_obj():
+    print("making object")
+    ob=Employee()
+    print("object created")
+    return ob
+ob= create_obj()
